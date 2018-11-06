229f2982f0483b36aad1c0f405f94ba76cef47d3
sasuke <mayjoel471@gmail.com>
2018-11-05 18:24:31 -0600

    Correcion

--- server/server.js
+++ server/server.js
@@ -1,43 +1,18 @@
+require('../config/config');
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+app.use(require('../routes/usuario'));
+
+mongoose.connect('mongodb://localhost:27017/admin', { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw new err;
+    console.log('Bases de Datos ONLINE');
 });
 
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => res.send('Delete Usuario'));
-
-
-app.listen(port, () => console.log(`Escuchando desde el puerto ${port}`));
+app.listen(process.env.PORT, () => console.log(`Escuchando desde el puerto ${process.env.PORT}`));
